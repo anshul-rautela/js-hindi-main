4ca0f17fe63400348fd5cbabc5526eced7c9fa74
Anshul Rautela <anshulrautela123456789@gmail.com>
2024-09-16 13:23:06 +0530

    datatypes

---
+++ 5_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"    //treat all js code as newer version
+//alert("hi") it is used in browser (not nodejs)
+
+//Datatypes
+// number => range 2^53
+// BigInt
+// string
+// Boolean
+// null
+// undefined
+// symbol
+
+
+//object
+
+
+let a;  //value is undefined
+let b = null; // value is null
+console.log(typeof null)        //object
+console.log(typeof undefined)   //undefined
+
+/* undefined apne aap me hi ek type hai but null ek object hai*/
