67082365cadbf82bb4367584aacb5e87f03e61cb
Anshul Rautela <anshulrautela123456789@gmail.com>
2024-09-17 12:27:57 +0530

    type-conversion

---
+++ 6_conversion.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score)              //number
+let valueInNumber=Number(score)        //type conversion
+
+console.log(valueInNumber)             //33
+
+
+
+let score2 = "33a"                     
+let valueInNumber2=Number(score2)
+console.log(valueInNumber2)              //NaN
+/*
+NaN means not a no. (and its type is no.)
+
+
+or when score="aa"
+let a = Number(score)
+console.log(score)  //nan
+
+Bollean conversion
+"" => false
+"afjasf" => true (when string is full it gives true)
+
+
+*/ 
